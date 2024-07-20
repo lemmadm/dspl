8c9f8d34dec883d88e89041b429d6760c25e6439
LemmaIoT <49133272+lemmadm@users.noreply.github.com>
2024-07-20 16:35:25 +0100

    Update googlesheet.js

--- css/googlesheet.js
+++ css/googlesheet.js
@@ -1,1 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxCgeXIixvkEEOnXnbqb_smepBpezjkQ-witzlCWBhQXEh_-jNmQcXXpbV1lPm36PSazg/exec'
 
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
